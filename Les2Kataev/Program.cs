d5fcb1f66abb31508e73a714e9e1a171acb7bd89
Сергей <@DESKTOP-5JQRDG9>
2021-08-17 12:31:32 +0300

    fg

--- Les2Kataev/Program.cs
+++ Les2Kataev/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Linq;
 
 namespace Les2Kataev
 {
-    class Program
+    class Program  
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите первую строку: ");
+            string s1 = Convert.ToString(Console.ReadLine());
+            Console.Write("Введите вторуя строку: ");
+            string s2 = Convert.ToString(Console.ReadLine());
+            NewMethod(s1, s2);
+        }
+
+        private static void NewMethod(string s1, string s2)
+        {
+            if (
+
+                        s1.Select(Char.ToUpper).OrderBy(x => x).SequenceEqual(s2.Select(Char.ToUpper).OrderBy(x => x))
+                           )
+                Console.WriteLine("Символы в строках одинаковые");
+            else
+                Console.WriteLine("Символы в строках различны");
         }
     }
 }
